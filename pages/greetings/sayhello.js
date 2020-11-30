818daecf87c5be60ab09ae274ba4143b9878ce49
Pradeep Kumar <pradeepk.pdp@gmail.com>
2020-11-30 12:49:05 +0530

    add greetings

---
+++ pages/greetings/sayhello.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css'
+
+export default function SayHello() {
+    return(
+        <div className={styles.container}>
+            <h3>Hello!!</h3>
+        </div>
+    )
+}
---
+++ pages/greetings/sayhi.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css'
+
+export default function sayhi() {
+    return (
+        <div className={styles.container}>
+            <h3>Hi!!</h3>
+        </div>
+    )
+}
